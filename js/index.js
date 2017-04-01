528fd80477b078268f7f55c70cabda4940646818
az <1457800393@qq.com>
2017-04-01 16:51:26 +0800

    121222

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 window.addEventListener('load',function(){
      $('.wrapper_picture')[0].addEventListener('click',function(){
-     	alert(1)
+//   	$('.prount_wra').css('display','none');
+//   	$('.second_info').css('display','block');
+     	$('.wrapper_text').css('transform','rotate(360deg)').css('opacity','0');
+     	$('.wrapper_left').css('transform','translate(-100%)');
+     	$('.wrapper_right').css('transform','translate(100%)');
+     	$('.wrapper_pic_center').css('opacity','0')
      })
 	
 	
